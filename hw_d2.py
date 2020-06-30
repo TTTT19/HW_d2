c78c70d24df119e2150456c8024268fddbf67c92
TTTT19 <t.a.kamalov@gmail.com>
2020-06-30 16:34:41 +0300

    change2

--- hw_d2.py
+++ hw_d2.py
@@ -3,12 +3,10 @@ import datetime
 import copy
 import time
 import json
-from storage_for_acess_token import get_token
-# from storage_for_acess_token import access_token  # вернуть если не дает токен
+from storage_for_acess_token import access_token
 import re
 import psycopg2
 
-access_token = get_token() # убрать, если не дает токен
 
 def get_age(birthday):
     today = datetime.date.today()
@@ -23,30 +21,57 @@ def get_age(birthday):
 class users_vk:
     def __init__(self):
         # находим группы пользователя
-        groups = requests.get('https://api.vk.com/method/groups.get', {'access_token': access_token, 'v': 5.103})
-        groups_list = (groups.json()['response']['items'])
-        self.group_id_for_param = groups_list
+
+        # 1)Получаем ID пользователя
+        user_id = input('Введите ID пользователя, ник или нажмите Enter, чтобы выбрать себя: ')
+        if user_id == '':
+            user_id = requests.get('https://api.vk.com/method/users.get',
+                                   {'access_token': access_token, 'v': 5.103})
+        else:
+            user_id = requests.get('https://api.vk.com/method/users.get', {'access_token': access_token, 'user_ids':user_id, 'v': 5.103})
+        self.user_id = user_id.json()['response'][0]['id']
+
+
+        # Получаем группы:
+        groups = requests.get('https://api.vk.com/method/groups.get', {'access_token': access_token, 'user_id': self.user_id,'v': 5.103})
+        try:
+            groups_list = (groups.json()['response']['items'])
+            self.group_id_for_param = groups_list
+        except:
+            print("групп не найдено или они закрыты")
+            self.group_id_for_param = []
+
+
 
         # находим данные пользователя
         person_data = requests.get('https://api.vk.com/method/users.get',
-                                   {'access_token': access_token, 'v': 5.103,
+                                   {'access_token': access_token, 'user_ids':self.user_id,'v': 5.103,
                                     'fields': 'sex, bdate, city, interests,relation'})
         person_data_2 = (person_data.json()['response'])
         data = copy.copy(person_data_2)
         # День рождения
-        self.person_bday_year = get_age(datetime.datetime.strptime(data[0]['bdate'], '%d.%m.%Y'))
+        try:
+            self.person_bday_year = get_age(datetime.datetime.strptime(data[0]['bdate'], '%d.%m.%Y'))
+        except:
+            self.person_bday_year = int(input('Сколько вам полных лет? '))
+
         # Пол
         self.person_sex = data[0]['sex']
         if self.person_sex == 1:
             self.search_person_sex = 2
         else:
             self.search_person_sex = 1
+
         # Интересы
-        if data[0]['interests'] == '':
+        try:
+            if data[0]['interests'] == '':
+                interest = input("Укажите свои интересы: ")
+                self.insterests_list = re.split(r',| ', interest)
+            else:
+                self.insterests_list = re.split(r',| ', data[0]['interests'])
+        except:
             interest = input("Укажите свои интересы: ")
             self.insterests_list = re.split(r',| ', interest)
-        else:
-            self.insterests_list = re.split(r',| ', data[0]['interests'])
 
         # Город
         try:
@@ -55,11 +80,14 @@ class users_vk:
             self.person_city = 1  # из-за отсутствующей базы городов с ID - просто подставляю Москву
 
         # Семейный статус
-        self.person_relation = data[0]['relation']
+        try:
+            self.person_relation = data[0]['relation']
+        except:
+            self.person_relation = 0
 
     def search_people_with_hints(self):
         people_from_search = requests.get('https://api.vk.com/method/users.search',
-                                          {'access_token': access_token, 'v': 5.103, 'count': 10,
+                                          {'access_token': access_token, 'v': 5.103, 'count': 20,
                                            'fields': 'bdate, city, interests, relation', 'sex': self.search_person_sex,
                                            'city': self.person_city, 'age_from': self.person_bday_year, 'has_photo': 1,
                                            'age_to': (self.person_bday_year + 1)})
@@ -93,6 +121,11 @@ class users_vk:
             match_count = match_count + user['groups_match'] * 0.5 + user['match'] * 0.6
             if self.person_sex != self.search_person_sex:
                 match_count += 0.5
+            try:
+                if user['relation'] == 1 or user['relation'] == 6:
+                    match_count += 0.5
+            except:
+                pass
             user['match_count'] = match_count
         return top_20_list
 
@@ -104,10 +137,14 @@ class users_vk:
                                              'owner_id': top_10['id'],
                                              'extended': 1})
             top_photo_for_append = []
-            for photo in profile_pic_list.json()['response']['items']:
-                top_photo_for_append.append({'like': photo['likes']['count'], 'url': photo['sizes'][-1]['url']})
-            top_photo_for_append = sorted(top_photo_for_append, key=lambda likes: likes['like'], reverse=True)[:3]
-            top_10['top3photo'] = top_photo_for_append
+            try:
+                for photo in profile_pic_list.json()['response']['items']:
+                    top_photo_for_append.append({'like': photo['likes']['count'], 'url': photo['sizes'][-1]['url']})
+                top_photo_for_append = sorted(top_photo_for_append, key=lambda likes: likes['like'], reverse=True)[:3]
+                top_10['top3photo'] = top_photo_for_append
+            except:
+                top_10['top3photo'] = 'Фото нет или не загружается'
+
 
     def dump_into_json(self, top_10_list):
         with open("groups.json", "w", encoding="utf-8") as file:
@@ -140,12 +177,12 @@ class db_work:
         self.cur.execute("""
         create table if not exists Users_match(
         id integer not null PRIMARY KEY,
-        first_name character varying(100) not null,
-        last_name character varying(100) not null,
+        first_name character varying(200) not null,
+        last_name character varying(200) not null,
         bdate character varying(100),
-        city character varying(100),
+        city character varying(200),
         relation numeric(10,0),
-        interests character varying(100),
+        interests character varying(500),
         match numeric(10,0),
         groups_match numeric(10,0),
         match_count numeric(10,2),
@@ -170,7 +207,7 @@ class db_work:
 
 
 def vkinder():
-    # 1)Получаем токен от пользователя (пока не сделал)
+
 
     # 2) Создаем нашего пользователя получачем по нему все данные
 
@@ -184,6 +221,7 @@ def vkinder():
 
     # 7) Находим количество совпадений групп с пользователем
     count_for_list = 0
+    print('проверяем количество вхождений людей из поиска с группами пользователя')
     for user in top_20_list:
         time.sleep(0.5)
         count_for_list += 1
