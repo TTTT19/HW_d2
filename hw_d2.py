0d840641fabc1eed95bf3d7bcf4e7af530b96f53
TTTT19 <t.a.kamalov@gmail.com>
2020-06-30 15:14:59 +0300

    change2

--- hw_d2.py
+++ hw_d2.py
@@ -3,10 +3,12 @@ import datetime
 import copy
 import time
 import json
-from storage_for_acess_token import access_token
+from storage_for_acess_token import get_token
+# from storage_for_acess_token import access_token  # вернуть если не дает токен
 import re
 import psycopg2
 
+access_token = get_token() # убрать, если не дает токен
 
 def get_age(birthday):
     today = datetime.date.today()
@@ -34,14 +36,14 @@ class users_vk:
         # День рождения
         self.person_bday_year = get_age(datetime.datetime.strptime(data[0]['bdate'], '%d.%m.%Y'))
         # Пол
-        person_sex = data[0]['sex']
-        if person_sex == 1:
+        self.person_sex = data[0]['sex']
+        if self.person_sex == 1:
             self.search_person_sex = 2
         else:
             self.search_person_sex = 1
         # Интересы
         if data[0]['interests'] == '':
-            interest = 'книги, музыка'
+            interest = input("Укажите свои интересы: ")
             self.insterests_list = re.split(r',| ', interest)
         else:
             self.insterests_list = re.split(r',| ', data[0]['interests'])
@@ -78,11 +80,21 @@ class users_vk:
     def friend_is_member(self, user_ids):
         count_member_group = 0
         for groups_id in self.group_id_for_param:
+            time.sleep(0.4)
             groups = requests.get('https://api.vk.com/method/groups.isMember',
                                   {'access_token': access_token, 'v': 5.103, 'group_id': groups_id,
                                    'user_ids': user_ids['id']})
             count_member_group = count_member_group + groups.json()['response'][0]['member']
         user_ids['groups_match'] = count_member_group
+
+    def all_match(self, top_20_list):
+        for user in top_20_list:
+            match_count = 0
+            match_count = match_count + user['groups_match'] * 0.5 + user['match'] * 0.6
+            if self.person_sex != self.search_person_sex:
+                match_count += 0.5
+            user['match_count'] = match_count
+        return top_20_list
 
     def get_porfile_pic(self, top_10_list):
         for top_10 in top_10_list:
@@ -136,6 +148,7 @@ class db_work:
         interests character varying(100),
         match numeric(10,0),
         groups_match numeric(10,0),
+        match_count numeric(10,2),
         top3photo character varying(500));
         """)
 
@@ -145,9 +158,10 @@ class db_work:
         for top_10 in top_10_list:
             top_10['top3photo'] = list(map(lambda x: json.dumps(x), top_10['top3photo']))
             self.cur.execute(
-                "INSERT INTO Users_match (id, first_name, last_name, bdate, city,relation,interests,match,groups_match,top3photo) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING",
+                "INSERT INTO Users_match (id, first_name, last_name, bdate, city,relation,interests,match,groups_match,match_count,top3photo) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING",
                 (top_10['id'], top_10['first_name'], top_10['last_name'], top_10['bdate'], top_10['city']['title'],
-                 top_10['relation'], top_10['interests'], top_10['match'], top_10['groups_match'], top_10['top3photo']))
+                 top_10['relation'], top_10['interests'], top_10['match'], top_10['groups_match'],
+                 top_10['match_count'], top_10['top3photo']))
             self.conn.commit()
 
     def get_top_10_list(self):
@@ -166,45 +180,47 @@ def vkinder():
 
     list_for_sort = new_user.find_same_interests(new_user.search_people_with_hints())
 
-    # 5) Сортируем по количеству совпадений по полю 'Match', пока не понимаю как это сделать
-
-    list_for_sort = sorted(list_for_sort, key=lambda key: key['match'], reverse=True)
-
-    # 6) Отбираем первые 50 аккаунтов в отсортированном списке
-
-    top_20_list = list_for_sort[:20]
+    top_20_list = list_for_sort
 
     # 7) Находим количество совпадений групп с пользователем
-
+    count_for_list = 0
     for user in top_20_list:
         time.sleep(0.5)
+        count_for_list += 1
+        print(f'Выполнено {count_for_list} запросов из {len(top_20_list)}')
         new_user.friend_is_member(user)
 
-    # 8) Сортируем по количеству совпадений групп
+    # 8) Подсчитываем совпадения по нескольким полям и получаем match_count
 
-    top_20_list = sorted(top_20_list, key=lambda key: key['groups_match'], reverse=True)
+    top_20_list = new_user.all_match(top_20_list)
 
-    # 9) Отбираем первые 10 аккаунтов в отсортированном списке
+
+    # 9) Сортируем по количеству набранных балов
+
+    top_20_list = sorted(top_20_list, key=lambda key: key['match_count'], reverse=True)
+
+    # 10) Отбираем первые 10 аккаунтов в отсортированном списке
 
     top_10_list = top_20_list[:10]
 
-    # 10) ищем профильные фотографии/ внутри сортируем по количеству лайков и оставляем первые 3
+
+    # 11) ищем профильные фотографии/ внутри сортируем по количеству лайков и оставляем первые 3
 
     new_user.get_porfile_pic(top_10_list)
     new_user.check_all_data(top_10_list)
 
-    # 11) Выгружаем в json
+    # 12) Выгружаем в json
 
     new_user.dump_into_json(top_10_list)
     print('данные выгружены в файл json')
 
-    # 12) Записываем в базу
+    # 13) Записываем в базу
 
     work_with_base = db_work()
     work_with_base.create_db()
     work_with_base.add_top_10_list(top_10_list)
     print('данные выгружены в БД')
-    # work_with_base.get_top_10_list()
+    work_with_base.get_top_10_list()
     work_with_base.conn.close()
 
 
